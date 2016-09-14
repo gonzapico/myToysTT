7d2d5d7c52ce1da43947b0fe30e2102f607982ab
Gonzalo Fernández <gfernandez@elconfidencial.com>
2016-09-14 20:30:45 +0200

    Request to get the navigation defined to b executed by Retrofit

---
+++ data/src/main/java/com/gfp/data/MyToysAPIService.java
@@ -0,0 +1,16 @@
+package com.gfp.data;
+
+import com.gfp.data.entity.NavigationEntries;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+/**
+ * Created by gfernandez on 14/09/16.
+ */
+
+public interface MyToysAPIService {
+
+  @Headers("x-api-key:" + Config.API_KEY) @GET(Config.API_URL_SECURE + "/api/navigation")
+  Call<NavigationEntries> navigation();
+}
