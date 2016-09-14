050193e4a1792355d70f92b7dc9d0798ecfc6486
Gonzalo Fernández <gfernandez@elconfidencial.com>
2016-09-14 21:02:53 +0200

    Refactoring by changed names

--- data/src/main/java/com/gfp/data/di/CloudComponent.java
+++ data/src/main/java/com/gfp/data/di/CloudComponent.java
@@ -1,6 +1,6 @@
 package com.gfp.data.di;
 
-import com.gfp.data.repository.datasource.CloudConfigDataStore;
+import com.gfp.data.repository.datasource.CloudNavigationDataStore;
 import dagger.Component;
 import javax.inject.Singleton;
 
@@ -8,5 +8,5 @@ import javax.inject.Singleton;
  * Created by gfernandez on 28/07/16.
  */
 @Singleton @Component(modules = CloudModule.class) public interface CloudComponent {
-  void inject(CloudConfigDataStore cloudNewsDataStore);
+  void inject(CloudNavigationDataStore cloudNewsDataStore);
 }
