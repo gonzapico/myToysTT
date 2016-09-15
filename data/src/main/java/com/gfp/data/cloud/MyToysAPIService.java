1d8cbab718f433e3bc3c0b647cab74c5a4f83b31
Gonzalo Fernández <gfernandez@elconfidencial.com>
2016-09-15 09:02:06 +0200

    Changed to RxJava

--- data/src/main/java/com/gfp/data/cloud/MyToysAPIService.java
+++ data/src/main/java/com/gfp/data/cloud/MyToysAPIService.java
@@ -2,9 +2,9 @@ package com.gfp.data.cloud;
 
 import com.gfp.data.Config;
 import com.gfp.data.entity.NavigationEntries;
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import rx.Observable;
 
 /**
  * Created by gfernandez on 14/09/16.
@@ -13,5 +13,5 @@ import retrofit2.http.Headers;
 public interface MyToysAPIService {
 
   @Headers("x-api-key:" + Config.API_KEY) @GET(Config.API_URL_SECURE + "/api/navigation")
-  Call<NavigationEntries> navigation();
+  Observable<NavigationEntries> navigation();
 }
