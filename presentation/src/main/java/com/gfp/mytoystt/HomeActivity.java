8b8607b8bef6226be2e83c31988de313d84c4fcf
Gonzalo Fernández <gfernandez@elconfidencial.com>
2016-09-15 21:25:57 +0200

    Added everything required by DI

--- presentation/src/main/java/com/gfp/mytoystt/HomeActivity.java
+++ presentation/src/main/java/com/gfp/mytoystt/HomeActivity.java
@@ -1,21 +1,47 @@
 package com.gfp.mytoystt;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import butterknife.BindView;
+import com.gfp.mytoystt.di.HasComponent;
+import com.gfp.mytoystt.di.components.DaggerNavigationComponent;
+import com.gfp.mytoystt.di.components.NavigationComponent;
 import com.gfp.mytoystt.loadNavigation.NavigationEntriesModel;
-import java.util.List;
+import com.gfp.mytoystt.loadNavigation.NavigationPresenter;
+import javax.inject.Inject;
 
-public class HomeActivity extends BaseMTActivity implements
-    com.gfp.mytoystt.loadNavigation.NavigationView {
+public class HomeActivity extends BaseMTActivity
+    implements com.gfp.mytoystt.loadNavigation.NavigationView, HasComponent<NavigationComponent> {
+
+  @Inject NavigationPresenter mNavigationPresenter;
+  @BindView(R.id.llNavigation) LinearLayout llNavigation;
+  private NavigationComponent mNavigationComponent;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    this.initializeInjector();
+    this.getComponent().inject(this);
+    mNavigationPresenter.setmNavigationView(this);
+    mNavigationPresenter.getNavigationItems();
   }
 
   @Override protected int getLayoutResource() {
     return R.layout.activity_home;
   }
 
-  @Override public void loadNavigationDrawer(List<NavigationEntriesModel> listOfEntries) {
+  @Override public NavigationComponent getComponent() {
+    return mNavigationComponent;
+  }
 
+  private void initializeInjector() {
+    this.mNavigationComponent = DaggerNavigationComponent.builder()
+        .applicationComponent(getApplicationComponent())
+        .activityModule(getActivityModule())
+        .build();
+  }
+
+  @Override public void loadNavigationDrawer(NavigationEntriesModel listOfEntries) {
+    Log.d(String.valueOf(this), listOfEntries.getNavigationEntryModelList().size() + "");
   }
 }
