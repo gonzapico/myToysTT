943f8477bc091ae4da4ebfd9d3e6bd1bbbb7bf6e
Gonzalo Fernández <gfernandez@elconfidencial.com>
2016-09-16 00:05:36 +0200

    Added test to check if factory works well

--- data/src/test/java/com/gfp/data/repository/datasource/NavigationDataStoreFactoryTest.java
+++ data/src/test/java/com/gfp/data/repository/datasource/NavigationDataStoreFactoryTest.java
@@ -2,6 +2,7 @@ package com.gfp.data.repository.datasource;
 
 import com.gfp.data.ApplicationTestCase;
 import com.gfp.data.entity.NavigationEntries;
+import com.gfp.data.entity.mapper.NavigationEntityDataMapper;
 import com.gfp.data.repository.NavigationRepository;
 import org.junit.Before;
 import org.junit.Rule;
@@ -9,7 +10,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import retrofit2.Call;
+import rx.Observable;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -18,29 +19,31 @@ import static org.mockito.Mockito.verify;
  * Created by gfernandez on 14/09/16.
  */
 public class NavigationDataStoreFactoryTest extends ApplicationTestCase {
-  //@Rule public ExpectedException expectedException = ExpectedException.none();
-  //
-  //private NavigationRepositoryDomain navigationRepository;
-  //@Mock private NavigationDataStoreFactory mockNavigationDataStoreFactory;
-  //@Mock private ConfigEntityDataMapper mockConfigEntityDataMapper;
-  //@Mock private NavigationDataStore mockNavigationDataStore;
-  //@Mock private NavigationEntries mockEntityNavigation;
-  //@Mock private TopNotification mockTopNotification;
-  //
-  //@Before public void setUp() {
-  //  MockitoAnnotations.initMocks(this);
-  //  navigationRepository = new NavigationRepositoryDomain(mockNavigationDataStoreFactory, mockConfigEntityDataMapper);
-  //
-  //  given(mockNavigationDataStoreFactory.createCloudDataStore()).willReturn(mockConfigDataStore);
-  //}
-  //
-  //@Test public void testGetConfigurationHappyCase() {
-  //  NavigationEntries entityConfiguration = new NavigationEntries();
-  //  given(mockNavigationDataStore.navigationList()).willReturn(Call<NavigationEntries>.just(entityConfiguration));
-  //
-  //  navigationRepository.
-  //
-  //  verify(mockNavigationDataStoreFactory).createCloudDataStore();
-  //  verify(mockNavigationDataStore).navigationList();
-  //}
+  @Rule public ExpectedException expectedException = ExpectedException.none();
+
+  private NavigationRepository navigationRepository;
+  @Mock private NavigationDataStoreFactory mockNavigationDataStoreFactory;
+  @Mock private NavigationEntityDataMapper mockNavigationEntityDataMapper;
+  @Mock private NavigationDataStore mockNavigationDataStore;
+  @Mock private NavigationEntries mockEntityNavigation;
+
+  @Before public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    navigationRepository =
+        new NavigationRepository(mockNavigationDataStoreFactory, mockNavigationEntityDataMapper);
+
+    given(mockNavigationDataStoreFactory.createCloudDataStore()).willReturn(
+        mockNavigationDataStore);
+  }
+
+  @Test public void testGetConfigurationHappyCase() {
+    NavigationEntries entityConfiguration = new NavigationEntries();
+    given(mockNavigationDataStore.navigationList()).willReturn(
+        Observable.just(entityConfiguration));
+
+    navigationRepository.navigation();
+
+    verify(mockNavigationDataStoreFactory).createCloudDataStore();
+    verify(mockNavigationDataStore).navigationList();
+  }
 }
